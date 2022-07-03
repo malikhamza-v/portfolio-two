0b5e38aa8719f666c34ac049dbb897f0f8999b89
malik <malikhamza.v@gmail.com>
2022-07-03 07:11:05 -0700

    "final"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -65,7 +65,7 @@ export const Banner = () => {
                 >
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
-                    {`Hi! I'm Judy`}{" "}
+                    {`Hi! I'm Malik Hamza`}{" "}
                     <span
                       className="txt-rotate"
                       dataperiod="1000"
